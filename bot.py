3f4992bb73f496ca47b19c7c30b8f954d95a2d3b
XuanSon99 <nguyensonn124@gmail.com>
2023-02-27 17:49:29 +0700

    change

--- bot.py
+++ bot.py
@@ -4,6 +4,7 @@ import requests
 import json
 from types import SimpleNamespace
 import math
+import random
 
 kyc = "👨‍💻 Xác minh KYC"
 uytin = "💎 DS Uy tín"
@@ -15,7 +16,6 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     buttons = [[KeyboardButton(kyc), KeyboardButton(uytin)]]
 
     reply_markup = ReplyKeyboardMarkup(buttons, resize_keyboard=True)
-
     await context.bot.send_message(chat_id=update.effective_chat.id, text="Chào mừng bạn đến với <b>Chợ OTC VN</b>. Hãy chọn phương án bên dưới:", reply_markup=reply_markup, parse_mode=constants.ParseMode.HTML)
 
 
@@ -23,32 +23,17 @@ async def messageHandler(update: Update, context: ContextTypes.DEFAULT_TYPE) -> 
     username = update.effective_user.username
     chat_id = update.effective_chat.id
 
-    if update.message.chat.username in ["minatabar", "ChoOTCVN_support"]:
+    # if update.message.chat.username in ["minatabar", "ChoOTCVN_support"]:
 
-        # if "/postwithbutton" in update.message.text:
-        #     text = update.message.text.split("|")
+    # if "/postwithbutton" in update.message.text:
+    #     text = update.message.text.split("|")
 
-        #     reply_markup = InlineKeyboardMarkup(
-        #         [[InlineKeyboardButton(
-        #             text=text[2], url="https://t.me/ChoOTCVN_bot")]],
-        #     )
+    #     reply_markup = InlineKeyboardMarkup(
+    #         [[InlineKeyboardButton(
+    #             text=text[2], url="https://t.me/ChoOTCVN_bot")]],
+    #     )
 
-        #     await context.bot.send_message(chat_id="-1001871429218", text=text[1], reply_markup=reply_markup, parse_mode=constants.ParseMode.HTML)
-
-        if "/post" in update.message.text:
-
-            text = "<b>Chọn thông báo bên dưới 🔥</b>\n<i>1. Thành viên uy tín là ai?\n2. KYC để làm gì?\n3. Affiliate - Chính sách hỗ trợ kết nối.\n4. Cảnh báo lừa đảo.\n5. Miễn trừ trách nhiệm.\n6. Uy Tín làm nên Thương hiệu.</i>"
-
-            reply_markup = InlineKeyboardMarkup(
-                [[InlineKeyboardButton(text='1 ☀️', callback_data='post1'),
-                  InlineKeyboardButton(text='2 🌩', callback_data='post2'), ],
-                 [InlineKeyboardButton(text='3 🌦', callback_data='post3'),
-                  InlineKeyboardButton(text='4 🔥', callback_data='post4'), ],
-                 [InlineKeyboardButton(text='5 ❄️', callback_data='post5'),
-                  InlineKeyboardButton(text='6 🌤', callback_data='post6'), ]],
-            )
-
-            await context.bot.send_message(chat_id, text=text, reply_markup=reply_markup, parse_mode=constants.ParseMode.HTML)
+    #     await context.bot.send_message(chat_id="-1001871429218", text=text[1], reply_markup=reply_markup, parse_mode=constants.ParseMode.HTML)
 
     if update.message.chat.type != "private":
 
@@ -195,43 +180,46 @@ async def button(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
 
         return
 
-    # choose daily post
-    if query.data == "post1":
-        text = "<b>Thành Viên Uy Tín Là Ai ?</b>\nLà những thành viên buôn bán thâm niên, chuyên nghiệp, có uy tín cao trong cộng đồng. Huy hiệu uy tín phải được đội ngũ bản quản lý chợ cấp.\n<b>Làm thế nào để trở thành TV uy tín ?</b>\n- Có trên 6 tháng hoạt động buôn bán tại Chợ OTC VN.\n- Giao dịch thành công tối thiểu 30 lần.\n- Được check thông tin cụ thể và phê duyệt từ 3 Admin.\n\n<i>Chat /uytin với bot @ChoOTCVN_bot để kiểm tra danh sách uy tín!</i>"
-        msg = "Đã gửi 1 ☀️"
-
-    if query.data == "post2":
-        text = "<b>Xác Minh Danh Tính (KYC) Để Làm Gì?</b>\nKYC là quy trình dành cho các User muốn mua bán và giao dịch thường xuyên trên chợ:\n- KYC để đẩy nhanh tiến độ giao dịch nếu bạn là khách hàng\n- KYC để xác minh uy tín nếu bạn là Merchant\n- KYC sớm để nhận những ưu đãi từ đội ngũ Admin chợ\n\n<i>Chat ngay với bot @ChoOTCVN_bot để KYC!</i>"
-        msg = "Đã gửi 2 🌩"
-
-    if query.data == "post3":
-        text = "<b>Affiliate - Chính Sách Hỗ Trợ Kết Nối</b>\nChợ OTC Việt Nam vẫn tiếp tục chạy các chương trình hỗ trợ khách hàng ngoài luồng OTC. Để nhận hoa hồng từ chương trình Affiliate, bạn hãy:\n<i>- Lập Gr chat của Merchant + khách hàng (phải có sự đồng ý thông qua Admin chợ)\n- Hỗ trợ quy trình giao dịch của khách hàng và Merchant</i>\n👉 Sau mỗi giao dịch, đội ngũ Admin sẽ check quy trình và hoàn lại cho các bạn 10% lợi nhuận.\n\n<i>Liên hệ @QuocPham_OTC để nhận thêm thông tin chi tiết!</i>"
-        msg = "Đã gửi 3 🌦"
-
-    if query.data == "post4":
-        text = "<b>Cảnh Báo Lừa Đảo (Scam Warning)</b>\nCó nhiều đối tượng sử dụng tài khoản Telegram với Bio + Username tương tự đội ngũ Admin và Merchant trong chợ nhắn tin cho người dùng để thực hiện hành vi lừa đảo.\n<b>Lưu ý:</b>\n- Đội ngũ Merchant Uy tín sẽ không chủ động nhắn tin cho các bạn để yêu cầu giao dịch\n- Số tài khoản - địa chỉ ví của Merchant được Admin chợ quản lý nghiêm ngặt, tuyệt đối không giao dịch thông qua các số tài khoản - địa chỉ ví lạ\n- Các kiểu tin nhắn như “chuyển gấp; ứng trước; chuyển qua cho bạn;....” đều là hình thức Scam trá hình\n\n<i>Liên hệ @ChoOTCVN_support để được giải đáp các thắc mắc trên chợ</i>"
-        msg = "Đã gửi 4 🔥"
-
-    if query.data == "post5":
-        text = "<b>Thông Báo Miễn Trừ Trách Nhiệm</b>\n- Chúng tôi sẽ không chịu trách nhiệm cho bất kỳ giao dịch ngoài luồng nào được thực hiện không thông qua Admin chợ!\n- Merchant của chợ sẽ không bao giờ chủ động nhắn tin để yêu cầu giao dịch!\n\n<i>Liên hệ @ChoOTCVN_support để được giải đáp các thắc mắc trên chợ</i>"
-        msg = "Đã gửi 5 ❄️"
-
-    if query.data == "post6":
-        text = "<b>Uy Tín Làm Nên Thương Hiệu</b>\nĐộ an toàn và lợi nhuận/giao dịch của các bạn sẽ luôn được đảm bảo khi giao dịch với Thương nhân có uy tín cao trong cộng đồng. Những thương nhân an toàn sẽ được gắn huy hiệu “Uy tín” khi đăng bài mua bán.\n\n<i>Chat /uytin với bot @ChoOTCVN_bot để kiểm tra danh sách uy tín!</i>"
-        msg = "Đã gửi 6 🌤"
-
-    await context.bot.send_message(chat_id="-1001871429218", text=text, parse_mode=constants.ParseMode.HTML)
-    await context.bot.send_message(chat_id, text=msg, parse_mode=constants.ParseMode.HTML)
-
-
-app = ApplicationBuilder().token(
-    "5960653063:AAHyOV3a4nndUwSyXc0Vkrh8Dq87LZ3dh00").build()
 
 # app = ApplicationBuilder().token(
-#     "6217705988:AAEOYp5g31rkl-iWrXAGE_mo7t0f0Oz3qIo").build()
+#     "5960653063:AAHyOV3a4nndUwSyXc0Vkrh8Dq87LZ3dh00").build()
+
+app = ApplicationBuilder().token(
+    "6217705988:AAEOYp5g31rkl-iWrXAGE_mo7t0f0Oz3qIo").build()
 
 app.add_handler(CommandHandler("start", start))
 app.add_handler(CallbackQueryHandler(button))
 app.add_handler(MessageHandler(filters.ALL, messageHandler))
+
+
+# auto send message
+async def callback_minute(context: ContextTypes.DEFAULT_TYPE):
+
+    list = [
+        "<b>Thành Viên Uy Tín Là Ai ?</b>\nLà những thành viên buôn bán thâm niên, chuyên nghiệp, có uy tín cao trong cộng đồng. Huy hiệu uy tín phải được đội ngũ bản quản lý chợ cấp.\n<b>Làm thế nào để trở thành TV uy tín ?</b>\n- Có trên 6 tháng hoạt động buôn bán tại Chợ OTC VN.\n- Giao dịch thành công tối thiểu 30 lần.\n- Được check thông tin cụ thể và phê duyệt từ 3 Admin.\n\n<i>Chat /uytin với bot @ChoOTCVN_bot để kiểm tra danh sách uy tín!</i>",
+
+        "<b>Xác Minh Danh Tính (KYC) Để Làm Gì?</b>\nKYC là quy trình dành cho các User muốn mua bán và giao dịch thường xuyên trên chợ:\n- KYC để đẩy nhanh tiến độ giao dịch nếu bạn là khách hàng\n- KYC để xác minh uy tín nếu bạn là Merchant\n- KYC sớm để nhận những ưu đãi từ đội ngũ Admin chợ\n\n<i>Chat ngay với bot @ChoOTCVN_bot để KYC!</i>",
+
+        "<b>Cảnh Báo Lừa Đảo (Scam Warning)</b>\nCó nhiều đối tượng sử dụng tài khoản Telegram với Bio + Username tương tự đội ngũ Admin và Merchant trong chợ nhắn tin cho người dùng để thực hiện hành vi lừa đảo.\n<b>Lưu ý:</b>\n- Đội ngũ Merchant Uy Tín sẽ không chủ động nhắn tin cho các bạn để yêu cầu giao dịch\n- Số tài khoản - địa chỉ ví của Merchant được Admin chợ quản lý nghiêm ngặt, tuyệt đối không giao dịch thông qua các số tài khoản - địa chỉ ví lạ\n- Các kiểu tin nhắn như “chuyển gấp; ứng trước; chuyển qua cho bạn;....” đều là hình thức Scam trá hình\n\n<i>Liên hệ @ChoOTCVN_support để được giải đáp các thắc mắc trên chợ</i>",
+
+        "<b>Thông Báo Miễn Trừ Trách Nhiệm</b>\n- Chúng tôi sẽ không chịu trách nhiệm cho bất kỳ giao dịch ngoài luồng nào được thực hiện không thông qua Admin chợ!\n- Merchant của chợ sẽ không bao giờ chủ động nhắn tin để yêu cầu giao dịch!\n\n<i>Liên hệ @ChoOTCVN_support để được giải đáp các thắc mắc trên chợ</i>",
+
+        "<b>Giới thiệu Chợ OTC Việt Nam</b>\nChợ OTC Việt Nam là cộng đồng giao lưu, trao đổi USDT,BTC, ETH,... các mặt hàng trực tiếp giữa tất cả <b>Khách Hàng</b> và các <b>Thương Nhân</b>, không thông qua bất cứ đơn vị tổ chức nào, không thu bất cứ khoản phí nào. Tất cả các phát sinh giao dịch đều là thỏa thuận giữa người mua và người bán.\n(Lưu ý: Chỉ các <b>Thương Nhân</b> mới được phép đăng quảng cáo trên chợ sau khi đăng ký trở thành <b>Thương Nhân</b> với BQL chợ.)\n\n<i>Liên hệ với @ChoOTCVN_support để nhận thông tin hỗ trợ</i>"]
+
+    res = requests.get(
+        f"{domain}/api/setup")
+    last_msg_id = res.json()[0]["value"]
+
+    # await context.bot.delete_message(message_id=last_msg_id, chat_id='-1001871429218')
+
+    msg = await context.bot.send_message(chat_id='-1001871429218', text=random.choice(list), parse_mode=constants.ParseMode.HTML)
+    
+    requests.put(
+        f"{domain}/api/setup/1", {'value': msg.message_id})
+
+
+job_queue = app.job_queue
+
+job_minute = job_queue.run_repeating(callback_minute, interval=600, first=10)
 
 app.run_polling()
